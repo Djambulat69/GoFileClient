a254b727222be3e9d0df1061fb9f8858cc532585
Djambulat <Djambulat69@gmail.com>
2021-06-12 23:48:51 +0300

    Init

---
+++ app/src/main/java/com/djambulat69/gofileclient/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.djambulat69.gofileclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
